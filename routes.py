6e21ff7347b776d87e6c1f5a887907cccbf1bc57
andrejsloboda <88687893+andrejsloboda@users.noreply.github.com>
2022-04-08 01:12:08 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,31 +1,21 @@
 from __future__ import annotations
+from datetime import datetime
+from typing import List
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from graph import Node
 
 
 class RouteOutput:
-    def __init__(self, route: 'Route', origin: str, destination: str, bag_count: int):
+    def __init__(self,route: 'Route', bag_count: int) -> None:
         self.route = route
-        self.origin = origin
-        self.destination = destination
+        self.origin = route.origin
+        self.destination = route.destination
         self.bag_count = bag_count
 
     @staticmethod
-    def _get_route_travel_time(route: 'Route') -> str:
-        """ This function calculates total travel time for route """
-
-        start_time = route.nodes[0].departure
-        end_time = route.nodes[-1].arrival
-        travel_time = str(end_time - start_time)
-    
-        return travel_time
-
-    @staticmethod
     def _get_route_total_price(route: 'Route', bag_count: int) -> float:
-        """ This function calculates total price for route.
-        The price depends on number of requested bags."""
-
+        
         if bag_count == 0:
             total_price = sum(float(node.base_price) for node in route.nodes)
             return total_price
@@ -42,17 +32,19 @@ class RouteOutput:
             "bags_allowed": min([int(node.bags_allowed) for node in self.route.nodes]),
             "bags_count": self.bag_count,
             "total_price": self._get_route_total_price(self.route, self.bag_count),
-            "travel_time": self._get_route_travel_time(self.route)
+            "travel_time": str(self.route._travel_time)
         }
 
 
 class Route:
-    def __init__(self, nodes: list[Node]) -> None:
+    def __init__(self, nodes: List[Node]) -> None:
         self._nodes = nodes
         self._visited_airports = list()
+        self._travel_time = None
+        self._get_route_travel_time()
         self._get_visited_airports()
     
-    def is_valid_node(self, node):
+    def is_valid_node(self, node: Node) -> bool:
         if node.destination not in self._visited_airports:
             return True
         else:
@@ -63,17 +55,23 @@ class Route:
             self._visited_airports.append(node.origin)
             self._visited_airports.append(node.destination)
 
+    def _get_route_travel_time(self) -> datetime:
+
+        start_time = self._nodes[0].departure
+        end_time = self._nodes[-1].arrival
+        self._travel_time = end_time - start_time
+  
     @property
-    def last_airport(self):
+    def destination(self):
         return self._nodes[-1].destination
+
+    @property
+    def origin(self):
+        return self._nodes[0].origin
 
     @property
     def last_node(self):
         return self._nodes[-1]
-
-    @property
-    def first_node(self):
-        return self._nodes[0]
 
     @property
     def nodes(self):
